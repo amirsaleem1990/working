c1a289124f3c25f0351d538564c83658b3ff1334
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-07-26 09:20:32 +0500

    updated

---
+++ Facebook_posts_links/.ipynb_checkpoints/facebook-links-checkpoint.py
@@ -0,0 +1,63 @@
+import time
+import pickle
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import numpy as np
+import time
+from selenium.webdriver.common.keys import Keys
+
+with open("../../Amir-personal/facebook-userName-and-password.txt", "r") as file:
+	usrname, pas = file.read().splitlines()
+    
+browser = webdriver.Firefox(executable_path="./geckodriver")
+#navigates you to the facebook page.
+browser.get('https://www.facebook.com/')
+
+#find the username field and enter the email example@yahoo.com.
+time.sleep(np.random.randint(3, 6))
+username = browser.find_elements_by_css_selector("input[name=email]")
+username[0].send_keys(usrname)
+
+
+#find the password field and enter the password password.
+time.sleep(np.random.randint(3, 6))
+password = browser.find_elements_by_css_selector("input[name=pass]")
+password[0].send_keys(pas)
+
+
+#find the login button and click it.
+time.sleep(np.random.randint(3, 6))
+loginButton = browser.find_elements_by_css_selector("input[type=submit]")
+loginButton[0].click()
+
+link_dict = {}
+for name, url in zip(["Mushtaq", "Asif mehmood", "Zahid mughal", "Mohammad Fahad Haris", "Abdullah Adam", "Hm Zubair", "Muhammad Imran", "Munib Hussain", "Jameel Baloch",
+                     "Rizwan Asad Khan", "Abubakr Quddusi", "Mohammad Din Jauhar", "Riayatullah Farooqui", "Asim AllahBakhsh", "Sohaib naseem", "Idrees Aazad"], 
+                     ["https://web.facebook.com/MMushtaqYusufzai", "https://web.facebook.com/asif.mahmood.1671", "https://web.facebook.com/zahid.mughal.5895", 
+                     "https://web.facebook.com/mohammad.f.haris", "https://web.facebook.com/abdullah.adam49", "https://web.facebook.com/hm.zubair.52", 
+                      "https://web.facebook.com/abumaryam82", "https://web.facebook.com/munib.hussain86", "https://web.facebook.com/jameelbaloch1924", 
+                      "https://web.facebook.com/theguided1", "https://web.facebook.com/abubakr.quddusi.3",
+                     "https://web.facebook.com/mohammaddin.jauhar.7", "https://web.facebook.com/Riayat.Farooqui", "https://web.facebook.com/asim.allahbakhsh",
+                     "https://web.facebook.com/sohaib.naseem.3", "https://web.facebook.com/idreesazaad"]):
+    if not name in link_dict:
+        link_dict[name] = []
+    browser.get(url)
+    s = BeautifulSoup(browser.page_source, "lxml")
+    a = s.find("div", {"id" : "timeline_story_column"})
+    for i in a.find_all('a'):
+        try:
+            if i['href'].startswith(url + "/post"):
+                link_dict[name].append(i['href'])
+        except:
+            pass
+
+with open("All_FB_links.pkl", "rb") as file:
+    all_links = pickle.load(file)
+for k,v in link_dict.items():
+    if not k in all_links:
+        all_links[k] = v
+    else:
+        all_links[k] = list(set(all_links[k] + v))
+time.sleep(30)
+browser.close()
