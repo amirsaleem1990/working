e2b211d951866645b0fdc9abf0ac39fbe3debfa0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-07-31 16:58:34 +0500

    updated

--- Facebook_posts_links/Open_FB_links.py
+++ Facebook_posts_links/Open_FB_links.py
@@ -5,9 +5,9 @@ with open("current_data.pkl", "rb") as file:
     current_data = pickle.load(file)
 
 
-for links_list in current_data.values():
-    for link in links_list:
-        os.system("firefox "+  link)
+# for links_list in current_data.values():
+#     for link in links_list:
+#         os.system("firefox "+  link)
         
 with open("All_FB_links.pkl", "rb") as file:
     all_links = pickle.load(file)
@@ -19,3 +19,7 @@ for k,v in current_data.items():
         for i in v:
             if not i in all_links[k]:
                 all_links[k].append(i)
+
+
+with open("All_FB_links.pkl", "wb") as file:
+	pickle.dump(all_links, file)
