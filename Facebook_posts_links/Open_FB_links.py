e46c803426292f0f10f7c4aae7876345a7df4b1b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-08-01 14:57:44 +0500

    updated

--- Facebook_posts_links/Open_FB_links.py
+++ Facebook_posts_links/Open_FB_links.py
@@ -1,16 +1,24 @@
 import os
 import pickle
+import itertools
 
 with open("current_data.pkl", "rb") as file:
     current_data = pickle.load(file)
 
+with open("All_FB_links.pkl", "rb") as file:
+    all_links = pickle.load(file)
+
+
+all_current_dict_links = list(itertools.chain(*current_data.values()))
+all_all_links_dict_links = list(itertools.chain(*all_links.values()))
 
 for links_list in current_data.values():
     for link in links_list:
-        os.system("firefox "+  link)
+    	if not link in all_current_dict_links:
+	        # os.system("firefox "+  link)
+	        pass
         
-with open("All_FB_links.pkl", "rb") as file:
-    all_links = pickle.load(file)
+
 
 for k,v in current_data.items():
     if not k in all_links:
