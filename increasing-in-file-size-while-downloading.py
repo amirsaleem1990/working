5a4e1ae024f11eea7ce88de475be41fae8371eb3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-09-10 16:03:39 +0500

    updated

--- increasing-in-file-size-while-downloading.py
+++ increasing-in-file-size-while-downloading.py
@@ -6,8 +6,8 @@ s_start = int(s_start[0].split("\t")[0][:-1])
 privous_size = int()
 while True:
     n = datetime.datetime.now()
-    time.sleep(10)
+    time.sleep(60)
     s = !du -shc session* -B M| grep total
     s = int(s[0].split("\t")[0][:-1])
-    print(s - s_start, f"0{n.hour-12}:{n.minute}")
+    print(s - s_start,"MB",  f"0{n.hour-12}:{n.minute}")
     s_start = s
