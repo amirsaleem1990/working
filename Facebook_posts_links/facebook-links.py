9d7445948e2dd05fcc84b5a17994494db3dd8f9a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-09-26 11:14:18 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -16,7 +16,7 @@ now = datetime.datetime.now()
 with open("/home/amir/github/working/Facebook_posts_links/All_FB_links_names_corrected.pkl", "rb") as file:
     all_links = pickle.load(file)
 
-stroed_links_qty : sum([len(all_links[i]) for i in all_links])    
+stored_links_qty : sum([len(all_links[i]) for i in all_links])    
 
 all_links_list = []
 for i in all_links:
@@ -94,7 +94,7 @@ for fb in FB:
 	except:
 		pass
 
-stroed_links_qty = sum([len(all_links[i]) for i in all_links])
+links_qty_after_addition = sum([len(all_links[i]) for i in all_links])
 
 print("New links Qty: ", links_qty_after_addition - stroed_links_qty)
 
