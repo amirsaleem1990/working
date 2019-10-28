b12725b1b5d540c5e60c71dbba4598534f195475
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-10-28 18:42:19 +0500

    updated

--- Facebook_posts_links/links-pickle-to-df.py
+++ Facebook_posts_links/links-pickle-to-df.py
@@ -5,10 +5,8 @@ with open("All_FB_links_names_corrected.pkl", "rb") as file:
 dd = {}
 names = []
 for i in d:
-	lst = d[i]
-	c = 0
-	for z in lst:
-		c += 1
+	for z in d[i]:
+		names += [i] * len(z)
 		if isinstance(z, tuple):
 			if i in dd:
 				dd[i].append(z)
@@ -20,7 +18,6 @@ for i in d:
 			else:
 				dd[i] = [(z, "TIME not recorded")]
 
-	names += [i] * c
 ls = [pd.DataFrame(dd[i]) for i in dd]
 
 df = pd.concat(ls).reset_index().drop("index", axis=1)
