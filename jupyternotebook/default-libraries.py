fbe658e07f746274de7a7be8849bd318711aa635
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-05 16:45:27 +0500

    updated

--- jupyternotebook/default-libraries.py
+++ jupyternotebook/default-libraries.py
@@ -1,3 +1,19 @@
 # go to ~/.ipython/profile_default/startup
 # create a file <start.py
 >
+# add these lines in the file:
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import re
+from datetime import datetime
+import pickle 
+import requests
+from bs4 import BeautifulSoup
+import pandas_profiling
+
+# Pandas options
+pd.options.display.max_columns = 30
+pd.options.display.max_rows = 20
+
