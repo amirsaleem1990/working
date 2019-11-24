fe21504c784e0dc24b646a7844fdbe466d8c04cc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-11-24 16:50:37 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -42,7 +42,7 @@ with open(home + "/github/Amir-personal/facebook-userName-and-password.txt", "r"
 print("Attempting to Login", current_time())
 
 # browser = webdriver.Firefox(executable_path=home + "/github/working/Facebook_posts_links/geckodriver")
-browser = webdriver.Firefox(executable_path= home + "/github/working/Facebook_posts_links/geckodriver", options=options)
+browser = webdriver.Firefox(executable_path = home + "/github/working/Facebook_posts_links/geckodriver", options=options)
 #navigates you to the facebook page.
 browser.get('https://www.facebook.com/')
 
