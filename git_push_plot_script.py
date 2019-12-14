b2ec1d661ff87a16b7e370a0c0e1a53a1e09c8ed
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-15 01:21:20 +0500

    updated

--- git_push_plot_script.py
+++ git_push_plot_script.py
@@ -5,19 +5,14 @@ df=pd.read_csv('~/git_counts.csv')
 df = df[df.Count > 0].sort_values('Count', ascending=False).reset_index().drop('index', axis=1)
 pprint.pprint(df)
 print(f'\n****  Total count: {df.Count.sum()} ****\n')
-# plot = plt.bar(df.Repo, df.Count)
-# plt.tick_params(axis='x', labelsize=14)
-# if len(df) > 9:
-# 	plt.xticks(rotation=45, ha='center', va='center', rotation_mode='anchor')
+plot = plt.bar(df.Repo, df.Count)
+plt.tick_params(axis='x', labelsize=14)
+if len(df) > 9:
+	plt.xticks(rotation=45, ha='center', va='center', rotation_mode='anchor')
 
-# for value in plot:
-# 	height = value.get_height()
-# 	plt.text(value.get_x() + value.get_width()/2., 1.002*height,'%d' % int(height), ha='center', va='bottom')
-# mng = plt.get_current_fig_manager()
-# mng.full_screen_toggle()
-# plt.show()
-
-df["Ratio"] = df.Count / df.Count.sum() * 100
-for i in df.iterrows():
-	row = i[1]
-	print(row['Repo'], "  ", "|" * int(row["Ratio"]))
+for value in plot:
+	height = value.get_height()
+	plt.text(value.get_x() + value.get_width()/2., 1.002*height,'%d' % int(height), ha='center', va='bottom')
+mng = plt.get_current_fig_manager()
+mng.full_screen_toggle()
+plt.show()
