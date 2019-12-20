66e3cda8ff38c985b21c398bc6a137566519715e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-20 16:34:38 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -221,7 +221,7 @@ try:
 except:
 	pass
 
-os.system("/home/amir/github/working/Facebook_posts_links/last_post.py")
+os.system("ipython3 /home/amir/github/working/Facebook_posts_links/last_post.py")
 
 # import os
 # import time
