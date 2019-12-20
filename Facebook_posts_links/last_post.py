a26f7553d1b7da21fa56c7df0acd9a31ba56233f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-20 16:23:07 +0500

    updated

--- Facebook_posts_links/last_post.py
+++ Facebook_posts_links/last_post.py
@@ -1,15 +1,30 @@
 # see days since last post 
+from tabulate import tabulate
+import os
+import pandas as pd
+import datetime
+os.chdir("/home/amir/github/working/Facebook_posts_links/")
 os.system("ipython3 links-pickle-to-df.py")
 previos_data = pd.read_csv("All_FB_links_names_corrected.csv")
-
 crnt_time = pd.to_datetime(
 	datetime.datetime.now(),
 	infer_datetime_format=True)
 
-	last_date = pd.to_datetime(
-		previos_data[previos_data.Name == fb].tail(1).Tate.values,
-			infer_datetime_format=True)
-	last_post_was_before_days = abs(int(str(list((last_date - crnt_time))[0]).split()[0]))
-	if not len(last_post_was_before_days):
-		last_post_was_before_days = "NA"
-	LPM.append((fb, last_post_was_before_days))
+diff = []
+for fb in  previos_data.Name.unique():
+    last_date = pd.to_datetime(
+        previos_data[previos_data.Name == fb].tail(1).Tate.values,
+        infer_datetime_format=True)
+    diff.append(str(list((crnt_time - last_date))[0]).split()[0])
+    
+df_diff = pd.DataFrame([previos_data.Name.unique(), diff]).T
+df_diff.columns = ["Name", "No post since"]
+df_diff["No post since"] = df_diff["No post since"].astype(int)
+df_diff2 = df_diff[df_diff["No post since"] > 0].sort_values("No post since", ascending=False)
+
+df_diff2["Link"] = "https://web.facebook.com/" + df_diff2.Name
+df_diff2 = df_diff2.reset_index().drop("index", axis=1)
+df_diff2.index = df_diff2.index + 1
+
+print(tabulate(pd.concat([pd.DataFrame(df_diff2.columns, index = list(df_diff2.columns)).T, 
+           df_diff2])))
