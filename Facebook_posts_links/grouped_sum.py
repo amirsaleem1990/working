3c2e3e2b6e352f15435d3a1062cae6d4b7213d27
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:31:06 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -2,10 +2,10 @@ import pandas as pd
 import time
 import os
 while True:
-	time.sleep(1)
+	time.sleep(60)
 	print("*****************************************************************")
 	os.system("date +%H:%M:%S")
-	os.system("echo -n $(du -c -BM *.pkl | grep total | cut -d 'M' -f 1) MB")
+	os.system("echo $(du -c -BM *.pkl | grep total | cut -d 'M' -f 1) MB")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
 	pkls = pd.Series(pkls)
