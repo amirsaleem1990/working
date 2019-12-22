545126dd653058ebec4661b37b447d0bc540afed
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:11:37 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -3,7 +3,7 @@ import time
 import os
 while True:
 	time.sleep(10)
-	os.system("du -sh *.pkl")
+	os.system("du -ch *.pkl | grep total")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
 	pkls = pd.Series(pkls)
