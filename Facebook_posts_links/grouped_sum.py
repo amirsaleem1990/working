27463433699405696fcf92e43949b113aaa113d3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:14:15 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -3,6 +3,7 @@ import time
 import os
 while True:
 	time.sleep(1)
+	print"*****************************************************************"
 	os.system("du -ch *.pkl | grep total")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
