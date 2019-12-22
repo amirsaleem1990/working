f0688a67f5e2a91ccd2e07e60489cb29a1c6f1aa
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:26:51 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -5,7 +5,7 @@ while True:
 	time.sleep(1)
 	print("*****************************************************************")
 	os.system("date +%H:%M:%S")
-	os.system("du -ch *.pkl | grep total | cut -d 'M' -f 1")
+	os.system("echo $(du -ch *.pkl | grep total | cut -d 'M' -f 1)  MB")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
 	pkls = pd.Series(pkls)
