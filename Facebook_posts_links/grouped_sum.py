29aa46a65fb5cac507c3a0ebd5d37738e5592627
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:13:32 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -2,9 +2,9 @@ import pandas as pd
 import time
 import os
 while True:
-	time.sleep(10)
+	time.sleep(1)
 	os.system("du -ch *.pkl | grep total")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
 	pkls = pd.Series(pkls)
-	pkls.value_counts()
+	print(pkls.value_counts())
