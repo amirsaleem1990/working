1c3d37b7a127940b0567ab6cb46af5b6727d58f7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:44:32 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -8,5 +8,6 @@ while True:
 	os.system("echo $(du -c -BM *.pkl | grep total | cut -d 'M' -f 1) MB")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
 	pkls = [i[i.rfind("_")+1:].strip(".pkl") for i in pkls]
+	pkls = [i for i in pkls if i != "corrected"]
 	pkls = pd.Series(pkls)
 	print(pkls.value_counts())
