b13fc24090e28aebfefbc18115472c9091b0b6fc
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-12-22 17:14:57 +0500

    updated

--- Facebook_posts_links/grouped_sum.py
+++ Facebook_posts_links/grouped_sum.py
@@ -3,7 +3,7 @@ import time
 import os
 while True:
 	time.sleep(1)
-	print"*****************************************************************"
+	print("*****************************************************************")
 	os.system("date +%H:%M:%S")
 	os.system("du -ch *.pkl | grep total")
 	pkls = [i for i in os.listdir() if i.endswith(".pkl")]
