c6651d213e1b06ee0c64726ea531f34d92626180
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-02 13:31:12 +0500

    updated

--- ab.sh
+++ ab.sh
@@ -1,5 +1,6 @@
 current_date="2020-01-02"
 desired_date="2019-12-28"
 ab=`git log --date=iso --pretty=format:'%ad%x08%aN'`
-$ab | awk '$0 >= "2019-12-28" && $0 <= "2020-01-02"'
+echo $ab
+$ab | awk "$0 >= '2019-12-28' && $0 <= '2020-01-02'"
 # git log --date=iso --pretty=format:'%ad%x08%aN' | awk "$0 >= $desired_date && $0 <= $current_date" 
