c26cba9fdbbb5995dfbdc7f7d6cd33ec0d8b44e7
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-02 13:11:20 +0500

    updated

--- ab.sh
+++ ab.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# day=1
+day=1
 current_date=`date +%F`
 desired_date=`date --date=$day" day ago" +%F`
-echo $desired_date
 # git log --date=iso --pretty=format:'%ad%x08%aN' | awk '$0 >= "`$desired_date`" && $0 <= "`$current_date`"'
 
 # git log --date=iso --pretty=format:'%ad%x08%aN' | awk '$0 >= "2019-12-30" && $0 <= "2020-01-02"'
 
-# git log --date=iso --pretty=format:'%ad%x08%aN' | awk '$0 >= $desired_date && $0 <= $current_date'
+git log --date=iso --pretty=format:'%ad%x08%aN' | awk '$0 >= $desired_date && $0 <= $current_date'
