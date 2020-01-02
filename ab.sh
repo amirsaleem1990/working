bd6fde97670743a9b503a03e42731bb00a75fa66
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-02 14:45:49 +0500

    updated

--- ab.sh
+++ ab.sh
@@ -1,3 +1,4 @@
-current_date='2020-01-02'
-desired_date='2019-12-28'
+# current_date='2020-01-02'
+# desired_date='2019-12-28'
+current_date=`date +%F`
 git log --pretty='format:%H %an %ae %ai' | grep $current_date | wc -l
