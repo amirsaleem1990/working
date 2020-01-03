74d63e8bedc9c0061063e4960c4d0d06532c5068
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-03 15:52:41 +0500

    updated

--- duplicate.py
+++ duplicate.py
@@ -9,9 +9,9 @@ with open("files-with-complete-path.txt", "w") as file:
 
 with open("files-with-complete-path.txt", "r") as file:
     a = file.read().splitlines()
-    
+a = [i.strip().replace("_", "").replace("-", "").replace(" ", "") for i in a]
 files = []
-for i in a:                                            
+for i in a:
 	if i.startswith("./"):       
 	    i = i[2:]                   
 	files.append(i[i.rfind("/")+1:])
@@ -20,8 +20,12 @@ print(len(a) == len(files))
 import pandas as pd
 print(pd.Series(files).nunique())
 
-df = pd.DataFrame(a, files)
-df.to_csv("files_names.csv")
+df = pd.DataFrame([files, a]).T
+df.columns = ["File", "Path"]
+# freq = df.File.value_counts()
+# freq[freq > 1]
+duplicated = df[df.File.duplicated()].sort_values("File")
+duplicated.to_csv("files_names.csv")
 
 
 print("2 file Ganerated:\nfiles-with-complete-path.txt\nfiles_names.csv\n\n")
