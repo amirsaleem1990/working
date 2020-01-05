5b43e8bd7b613bf7f842f0362f2f9bdb730d1a65
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-01-05 16:45:56 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -50,8 +50,8 @@ while Successfully_logedin:
 	if Successfully_logedin_num > 1:
 		print(f"Attempt no. {Successfully_logedin_num} to Login")
 	try:
-		# browser = webdriver.Firefox(executable_path="/home/amir/github/working/Facebook_posts_links/geckodriver")
-		browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver", options=options)
+		browser = webdriver.Firefox(executable_path="/home/amir/github/working/Facebook_posts_links/geckodriver")
+		# browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver", options=options)
 		#navigates you to the facebook page.
 		browser.get('https://www.facebook.com/')
 
