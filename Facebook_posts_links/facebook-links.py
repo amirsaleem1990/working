34bedf1c211e9de22e22965e059f7e681c4a8152
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-02-06 17:37:59 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -194,7 +194,7 @@ for fb in FB:
 				links_ = a.select('a')
 				for i in links_:
 					link = i['href']
-					if link.startswith("https://www.facebook.com/"):
+					if (link.startswith("https://www.facebook.com/")) or (link.startswith("https://web.facebook.com/")):
 						if not link in str(all_links):
 							if "/posts/" in link: # failing to capture like this link (https://web.facebook.com/permalink.php?story_fbid=129882195152447&id=100043920022318)
 								if not "?comment_id=" in link:
