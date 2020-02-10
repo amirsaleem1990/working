0d3103a5aa69b2456153606f1494d12465a237a5
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-02-10 17:31:03 +0500

    updated

--- Facebook_posts_links/get_all_links_for_user/2/script.py
+++ Facebook_posts_links/get_all_links_for_user/2/script.py
@@ -52,40 +52,39 @@ while Successfully_logedin:
 errors = []
 df = pd.read_csv("/home/amir/github/working/Facebook_posts_links/get_all_links_for_user/1-facebook-users-all-posts-links/Links.csv")
 for unique_name in df.Name.unique():
-	if not unique_name in ["mohammad.f.haris", "Riayat.Farooqui", "theguided1", "hm.zubair.52", "munib.hussain86"]:
-		print("************************************************************************")
-		print(unique_name)
-		try:
-			local_df = df[df.Name == unique_name]
-			for i in local_df.iterrows():
-				name = i[1][0]
-				link = i[1][1]
-				file_name = f"{name}.txt"
+	print("************************************************************************")
+	print(unique_name)
+	try:
+		local_df = df[df.Name == unique_name]
+		for i in local_df.iterrows():
+			name = i[1][0]
+			link = i[1][1]
+			file_name = f"/home/amir/github/working/Facebook_posts_links/{name}.txt"
+			try:
+				existing = open(file_name).read()
+			except:
+				existing = ""
+			if not link in existing:
 				try:
-					existing = open(file_name).read()
+					time.sleep(1)
+					browser.get(link)
 				except:
-					existing = ""
-				if not link in existing:
-					try:
-						time.sleep(1)
-						browser.get(link)
-					except:
+					errors.append([name, link])
+					continue
+				try:
+					soup = BeautifulSoup(browser.page_source, "lxml")
+					aa = soup.find("div", {"class" : "_5wj-"}).text
+					if len(aa) > 0:
+						file = open(file_name, "a+")
+						file.write("\n" + "#"*30 + "\n")
+						file.write(link + "\n")
+						file.write(aa + "\n")
+						file.close()
+					else:
 						errors.append([name, link])
-						continue
-					try:
-						soup = BeautifulSoup(browser.page_source, "lxml")
-						aa = soup.find("div", {"class" : "_5wj-"}).text
-						if len(aa) > 0:
-							file = open(file_name, "a+")
-							file.write("\n" + "#"*30 + "\n")
-							file.write(link + "\n")
-							file.write(aa + "\n")
-							file.close()
-						else:
-							errors.append([name, link])
-					except:
-						errors.append([name, link])
-						pass
-		except:
-			print(f"Error: {name}")
+				except:
+					errors.append([name, link])
+					pass
+	except:
+		print(f"Error: {name}")
 
