f1406d7cc0f36ae1ec7b4c6d911e7c9f7519ec62
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-02-20 16:54:39 +0500

    updated

--- Facebook_posts_links/facebook-links.py
+++ Facebook_posts_links/facebook-links.py
@@ -125,7 +125,8 @@ FB = ["MMushtaqYusufzai", 					# Muhammad Mushtaq
 		"profile.php?id=100004721416966",	# Dr-Muhammad Shahbaz Manj
 		"profile.php?id=100010345081577",	# Ali Imran
 		"zeeshan1857",						# Zeeshan Waraich
-		"jamilasgharjaami"					# jamil asghar jaami 
+		"jamilasgharjaami",					# jamil asghar jaami 
+		"athar.w.azeem"						# Athar Waqar Azeem
 	  ]
 
 with open("ids_removed_from_facebook.pkl", "rb") as file:
