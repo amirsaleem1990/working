5354b7dc24c760901a4aacfe32e90291f96066a2
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-05-09 22:30:40 +0500

    updated

--- Facebook_posts_links/get_all_links_for_user/0-get-timeline-and-further-links/script.py
+++ Facebook_posts_links/get_all_links_for_user/0-get-timeline-and-further-links/script.py
@@ -36,46 +36,7 @@ stored_links_qty = sum([len(all_links[i]) for i in all_links])
 with open("/home/amir/github/Amir-personal/facebook-userName-and-password_3.txt", "r") as file:
 	usrname, pas = file.read().splitlines()
 
-print("Attempting to Login   ", current_time())
-Successfully_logedin = True
-Successfully_logedin_num = 0
-fb_base_url = "https://m.facebook.com/" # "https://web.facebook.com/"
-while Successfully_logedin:
-	Successfully_logedin_num += 1
-	if Successfully_logedin_num > 1:
-		print(f"Attempt no. {Successfully_logedin_num} to Login")
-	try:
-		browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver")
-		
-		#navigates you to the facebook page.
-		browser.get(fb_base_url)
-
-		#find the username field and enter the email example@yahoo.com.
-		time.sleep(np.random.randint(3, 6))
-		username = browser.find_elements_by_css_selector("input[name=email]")
-		username[0].send_keys(usrname)
-
-
-		#find the password field and enter the password password.
-		time.sleep(np.random.randint(3, 6))
-		password = browser.find_elements_by_css_selector("input[name=pass]")
-		password[0].send_keys(pas)
-
-
-		#find the login button and click it.
-		time.sleep(np.random.randint(3, 6))
-		loginButton = browser.find_elements_by_css_selector("input[type=submit]")
-		loginButton[0].click()
-        
-		#find the OK button and click it.
-		time.sleep(np.random.randint(3, 6))
-		okButton = browser.find_elements_by_css_selector("input[type=submit]")
-		okButton[0].click()
-
-		print("Successfully Logged in", current_time())
-		Successfully_logedin = False
-	except:
-		pass
+LOGIN()
     
 
 with open("/home/amir/github/working/Facebook_posts_links/ids_removed_from_facebook.pkl", "rb") as file:
