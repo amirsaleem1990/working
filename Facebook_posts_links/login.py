94857138e67edafe175a842f39d4290b672adbe0
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-11 16:03:50 +0500

    updated

--- Facebook_posts_links/login.py
+++ Facebook_posts_links/login.py
@@ -17,48 +17,50 @@ def current_time():
 		tt += ":"
 	tt = tt.strip(":")
 	return tt
+def LOGIN():
+	print("Attempting to Login   ", current_time())
+	Successfully_logedin = True
+	Successfully_logedin_num = 0
+	# if input("Are u need visual tracking? [y\\n]:\t").lower() == "y":
+		# visual = True
+	# else:
+	from selenium.webdriver.firefox.options import Options
+	options = Options()
+	options.add_argument("--headless")
+	visual = True
+		
+	while Successfully_logedin:
+		Successfully_logedin_num += 1
+		if Successfully_logedin_num > 1:
+			print(f"Attempt no. {Successfully_logedin_num} to Login")
+		try:
+			if visual:
+				browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver")
+			else:
+				browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver", options=options)	
 
-print("Attempting to Login   ", current_time())
-Successfully_logedin = True
-Successfully_logedin_num = 0
-# if input("Are u need visual tracking? [y\\n]:\t").lower() == "y":
-	# visual = True
-# else:
-from selenium.webdriver.firefox.options import Options
-options = Options()
-options.add_argument("--headless")
-visual = True
-	
-while Successfully_logedin:
-	Successfully_logedin_num += 1
-	if Successfully_logedin_num > 1:
-		print(f"Attempt no. {Successfully_logedin_num} to Login")
-	try:
-		if visual:
-			browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver")
-		else:
-			browser = webdriver.Firefox(executable_path = "/home/amir/github/working/Facebook_posts_links/geckodriver", options=options)	
+			#navigates you to the facebook page.
+			browser.get('https://www.facebook.com/')
 
-		#navigates you to the facebook page.
-		browser.get('https://www.facebook.com/')
-
-		#find the username field and enter the email example@yahoo.com.
-		time.sleep(np.random.randint(3, 6))
-		username = browser.find_elements_by_css_selector("input[name=email]")
-		username[0].send_keys(usrname)
-
-
-		#find the password field and enter the password password.
-		time.sleep(np.random.randint(3, 6))
-		password = browser.find_elements_by_css_selector("input[name=pass]")
-		password[0].send_keys(pas)
+			#find the username field and enter the email example@yahoo.com.
+			time.sleep(np.random.randint(3, 6))
+			username = browser.find_elements_by_css_selector("input[name=email]")
+			username[0].send_keys(usrname)
 
 
-		#find the login button and click it.
-		time.sleep(np.random.randint(3, 6))
-		loginButton = browser.find_elements_by_css_selector("input[type=submit]")
-		loginButton[0].click()
-		print("Successfully Logged in", current_time())
-		Successfully_logedin = False
-	except:
-		pass
+			#find the password field and enter the password password.
+			time.sleep(np.random.randint(3, 6))
+			password = browser.find_elements_by_css_selector("input[name=pass]")
+			password[0].send_keys(pas)
+
+
+			#find the login button and click it.
+			time.sleep(np.random.randint(3, 6))
+			loginButton = browser.find_elements_by_css_selector("input[type=submit]")
+			loginButton[0].click()
+			print("Successfully Logged in", current_time())
+			Successfully_logedin = False
+		except:
+			pass
+
+return browser
