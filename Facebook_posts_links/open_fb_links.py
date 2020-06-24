39dd62ee7fd7ea7e0d6d005f7c72eea44e996d1a
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-06-25 00:40:25 +0500

    updated

--- Facebook_posts_links/open_fb_links.py
+++ Facebook_posts_links/open_fb_links.py
@@ -3,6 +3,7 @@ import pickle
 import os
 file_ = "/home/amir/github/working/Facebook_posts_links/links_to_open.pkl"
 links_to_open = pickle.load(open(file_, "rb"))
+print(f"There is {len(links_to_open)} in the file\n")
 procecced = 0
 links_opened = []
 for i in links_to_open:
@@ -16,4 +17,8 @@ for i in links_to_open:
 		else:
 			procecced = 0
 links_to_open = [i for i in links_to_open if not i in links_opened]
+if not links_to_open:
+	print("All links opened\n")
+else:
+	print(f"There are still {len(links_to_open)} links to open in the file\n")
 pickle.dump(links_to_open, open(file_, "wb"))
