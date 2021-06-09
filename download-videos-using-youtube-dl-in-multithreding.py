06fcebdb9718e69918e9b893eb5d08185cf27cf1
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-06-09 17:33:30 +0500

    pehly dictionary ban rahi thi, magar us ka koi faida nahi tha, to me ny us ki list bana di h

--- download-videos-using-youtube-dl-in-multithreding.py
+++ download-videos-using-youtube-dl-in-multithreding.py
@@ -2,22 +2,35 @@
 # https://stackoverflow.com/questions/50197643/youtbe-dl-multiple-downloads-at-the-same-time
 import multiprocessing.dummy
 import subprocess
+import os
 
+os.remove("_Errors_.txt")
 
-arr = [{'url' : i} for i in set(open("mp4_links.txt", "r").read().splitlines()) if i.strip()]
+def download(url):
+	try:
+		subprocess.check_call([
+			'youtube-dl', url])
+		open("downloaded.txt", "a").write(url+"\n")
+	except:
+		print("\n\nERROR --------------------------------------------------")
+		print(f"url: {url}")
+		e_ = traceback.format_exc()
+		print(e_)
+		print("---------------------------------------------- Error END\n\n")
+
+		open("_Errors_.txt", 'a').wirte(f'---------------------------------\nUrl:{url}\n{e_}\n\n')
+
+try:
+	arr = [i for i in set(open("mp4_links.txt", "r").read().splitlines()) if i.strip()]
+except:
+	file_name = input("file <mp4_links.txt> not found, please Enter your file name: ")
+	arr = [i for i in set(open(file_name, "r").read().splitlines()) if i.strip()]
+
 try:
 	downloaded = open("downloaded.txt", "r").read().splitlines()
-	arr = [i for i in arr if not i['url'] in  downloaded]
+	arr = [i for i in arr if not i in downloaded]
 except:
 	pass
 	
-def download(v):
-	try:
-		subprocess.check_call([
-			'youtube-dl', v['url']])
-		open("downloaded.txt", "a").write(v['url']+"\n")
-	except:
-		pass
 p = multiprocessing.dummy.Pool()
 p.map(download, arr)
- 
